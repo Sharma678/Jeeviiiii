8a2da32d382c727361948b6e3458c795e9644ed6
Sharma678 <138674281+Sharma678@users.noreply.github.com>
2024-03-12 13:57:58 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf   
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
